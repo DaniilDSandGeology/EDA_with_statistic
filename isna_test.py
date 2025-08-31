da92411306d57326428ea8d7dc939e836c4eb40c
DaniilDSandGeology <Potapov9401@mail.ru>
2025-08-31 19:49:42 +0300

    first commit

---
+++ isna_test.py
@@ -0,0 +1,19 @@
+import panas as pd
+
+
+def isna_test(df):
+    """test of null
+
+    Args:
+        df (_type_): data frame
+    """
+    col_index = dict()
+    for col in df:
+        if df[col].isna().sum() > 0:
+            col_index[col] = df[col].isna().sum()
+        else:
+            pass
+    if len(col_index) == 0:
+        print('пропусков нет')
+    else:
+        return (pd.Series(col_index))
